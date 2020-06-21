fb7ce8bcd1d3f00ea7a0aaf5eca948e49a70a2cb
Bessss-zyw <1186449334@qq.com>
2020-06-21 15:35:27 +0800

    clear repo

--- src/main/java/com/reins/bookstore/repository/UserIconRepository.java
+++
@@ -1,8 +0,0 @@
-package com.reins.bookstore.repository;
-
-import com.reins.bookstore.entity.UserIcon;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-//@RepositoryRestResource(collectionResourceRel = "personicon", path = "personicon")
-public interface UserIconRepository extends MongoRepository<UserIcon, Integer> {
-}
